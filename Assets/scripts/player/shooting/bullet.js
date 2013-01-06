53f4b05938a2e3f66ebe303d4077beb2413789b1
Tuur Lievens <tuur.lievens@hotmail.com>
2013-01-06 21:35:25 +0100

    further shooting

--- Assets/scripts/player/shooting/bullet.js
+++ Assets/scripts/player/shooting/bullet.js
@@ -2,21 +2,22 @@
 private var shot:boolean = false;
 var player : Transform;
 
-
 function Start () {
-	yield WaitForSeconds(2);
 	shot = true;
 }
 
 function Update () {
 	if(shot){
 		transform.position.z += 30 * Time.deltaTime + 0.5;
-		transform.position.x = player.position.x;
 	}
 	
 	//Remove bullet if it hits nothing
 	if (transform.position.z > player.position.z + 150){
 		Destroy(gameObject);
+	}
+	// Shoots bullet at keypress
+	if (Input.GetButtonDown("Fire1")) {	
+		Instantiate(bullet, Vector3(transform.position.x,transform.position.y,transform.position.z+1), transform.rotation);
 	}
 }
 
