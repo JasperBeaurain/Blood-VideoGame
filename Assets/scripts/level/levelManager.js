99e3ef006efbeb95c76bdbac35d49a2f382b0c98
Jasper9041 <Jasper9041@tehgamingcrew.com>
2013-01-07 21:42:59 +0100

    Rewrote level generation, includes randomisation

--- Assets/scripts/level/levelManager.js
+++ Assets/scripts/level/levelManager.js
@@ -1,43 +1,61 @@
 #pragma strict
 import System.Collections.Generic;
 
-
 var numberOfparts:int = 0;
-var prefab:Transform;
 var recycleDuration:float;
+
+var prefab1:Transform;
+var prefab2:Transform;
+var prefab3:Transform;
+var prefab4:Transform;
+var prefab5:Transform;
 
 private var nextPos : Vector3;
 private var levelQueue : Queue.<Transform> = new Queue.<Transform>(numberOfparts);
+private var rand:int;
 
 function Start () {
 	
 	//Instantiate all the parts
+	nextPos = Vector3.zero;
 	var i:int;
 	for(i=0;i<numberOfparts;i++){
 		var o:Transform;
-		o = Instantiate(prefab,nextPos,prefab.rotation);
+		rand = Random.Range(1,6);
+		o = Instantiate(calcPrefab(rand),nextPos,calcPrefab(rand).rotation);
+		nextPos.z += o.localScale.z;
 		levelQueue.Enqueue(o);
-	}	
-	nextPos = transform.localPosition;
-	for(i=0;i<numberOfparts;i++){
-		recycle();
 	}
 }
 
 function Update () {
-	//check if recycling is needed
 	var o:Transform;
 	o = levelQueue.Peek();
 	if( o.localPosition.z + recycleDuration < playermovement.distTravelled){
-		recycle();
+		o =	levelQueue.Dequeue();
+		var deadObjLength:float;
+		deadObjLength = o.localScale.z;
+		Destroy(o.gameObject);
+		rand = Random.Range(1,6);
+		o = Instantiate(calcPrefab(rand),nextPos,calcPrefab(rand).rotation);
+		nextPos.z += deadObjLength;
+		levelQueue.Enqueue(o);
 	}
 }
 
-function recycle(){
-	//Move the closest level part to the end of the stack
-	var o: Transform;
-	o = levelQueue.Dequeue();
-	o.localPosition = nextPos;
-	nextPos.z += (o.localScale.z);	
-	levelQueue.Enqueue(o);
+function calcPrefab(number:int){
+	switch(number){
+			case 1:
+				return prefab1;
+			case 2:
+				return prefab1;
+			case 3:
+				return prefab2;
+			case 4:
+				return prefab3;
+			case 5:
+				return prefab4;
+			case 6:
+				return prefab5;
+		}
 }
