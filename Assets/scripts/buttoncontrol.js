58f9774b236cb313ead6568f92ad15c817ed6158
Tuur Lievens <tuur.lievens@hotmail.com>
2013-04-17 18:28:33 +0200

    cheats improvements & particle aesthetics

--- Assets/scripts/buttoncontrol.js
+++ Assets/scripts/buttoncontrol.js
@@ -7,6 +7,19 @@ var bullet : Transform;
 var shield : Transform;
 var Gui:Transform;
 
+	//Cheats keys caching 
+	
+	private var fdown:boolean=false;
+	private var adown:boolean=false;
+	private var sdown:boolean=false;
+	private var tdown:boolean=false;
+	
+	private var hdown:boolean=false;
+	private var idown:boolean=false;
+	private var edown:boolean=false;
+	private var ldown:boolean=false;
+	private var ddown:boolean=false;
+
 function Start () {
 	
 }
@@ -23,22 +36,60 @@ function Update () {
 	   			 }
 			}
 			//Cheats for android
-			if (Input.GetKeyDown(KeyCode.Menu)){
+			/*if (Input.GetKeyDown(KeyCode.Menu)){
 				if (Time.time - lastTime < 0.2){
 					SpeedyCheat();
 				}else {
 					ShieldCheat();
 					lastTime = Time.time;
 				}
-			}
+			}*/
 		}else{
-			//Cheats for windows
-			if (Input.GetKeyDown(KeyCode.W)){
-				SpeedyCheat();
-			}else if (Input.GetKeyDown(KeyCode.X)){
-				ShieldCheat();
-			}else if(Input.GetKeyDown(KeyCode.Escape)){
+			if(Input.GetKeyDown(KeyCode.Escape)){
 				Gui.GetComponent(GUIscript).SendMessage("Pause");
+			}
+			
+	//Cheats for windows
+			//speed FAST
+			else if (Input.GetKeyDown(KeyCode.F)){fdown=!fdown;}
+			else if (Input.GetKeyDown(KeyCode.A)){adown=!adown;}
+			else if (Input.GetKeyDown(KeyCode.S)){sdown=!sdown;}
+			else if (Input.GetKeyDown(KeyCode.T)){tdown=!tdown;}
+			
+			else if (fdown&&adown&&sdown&&tdown){
+				SpeedyCheat();
+				fdown=false;
+				adown=false;
+				sdown=false;
+				tdown=false;
+			}
+			//SHIELD
+			else if (Input.GetKeyDown(KeyCode.H)){hdown=!hdown;}
+			else if (Input.GetKeyDown(KeyCode.I)){idown=!idown;}
+			else if (Input.GetKeyDown(KeyCode.E)){edown=!edown;}
+			else if (Input.GetKeyDown(KeyCode.L)){ldown=!ldown;}
+			else if (Input.GetKeyDown(KeyCode.D)){ddown=!ddown;}
+			else if (sdown&&hdown&&idown&&edown&&ldown&&ddown){
+				ShieldCheat();
+				sdown=false;
+				hdown=false;
+				idown=false;
+				edown=false;
+				ldown=false;
+				ddown=false;
+			}
+			//reset all on press of R
+			else if (Input.GetKeyDown(KeyCode.R)){
+				fdown=false;
+				adown=false;
+				sdown=false;
+				tdown=false;
+				hdown=false;
+				idown=false;
+				edown=false;
+				ldown=false;
+				ddown=false;
+				player.GetComponent(playermovement).forwardSpeed = 15;
 			}
 		}
 	}else{
