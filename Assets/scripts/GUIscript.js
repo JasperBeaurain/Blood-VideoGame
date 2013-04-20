f65954a26b7d88f6876a1a3fa778dd80ee95d4f6
Jasper9041 <Jasper9041@tehgamingcrew.com>
2013-04-20 12:36:31 +0200

    finished pause & death menus

--- Assets/scripts/GUIscript.js
+++ Assets/scripts/GUIscript.js
@@ -5,7 +5,7 @@ var player : Transform;
 var health: int = 120;
 public static var score: int = 0;
 public static var totscore : int = 0;
-public static var playerName:String = "";
+public static var playerName:String = "Enter your name here!";
 var power1: int;
 var power2: int;
 var power3: int;
@@ -17,12 +17,18 @@ var cooldown: String;
 var style:GUIStyle;
 public static var paused = false;
 public static var dead = false;
+var isInstructions:boolean = false;
+var isMainMenu:boolean = false;
+var isOptions:boolean = false;
 
 var buttonStyle:GUIStyle;
+var instructionStyle:GUIStyle;
 var bgTexture:Texture;
 var titleStyle:GUIStyle;
 var scoreStyle:GUIStyle; 
 var guiScoreStyle:GUIStyle;
+var optionsStyle:GUIStyle;
+var textFieldStyle:GUIStyle;
 
 var guiBgTexture:Texture;
 
@@ -32,7 +38,11 @@ var power3Texture:Texture;
 var power4Texture:Texture;
 var power5Texture:Texture;
 
+private var textField:String = "";
+
 function Start () {
+	reset();
+	isMainMenu = true;
 }
 
 function Update () {
@@ -61,41 +71,57 @@ function OnGUI(){
 	if(paused){
 		
 		GUI.DrawTexture(Rect(0,0,800,480), bgTexture);
-		
-		GUI.Label(Rect(300,50,200,40),"Paused",titleStyle);
-		
-		if (GUI.Button(Rect(300,100,200,60),"Resume",buttonStyle)){
-	 		Pause();
-		}
-		if (GUI.Button(Rect(300,172,200,60),"Main Menu",buttonStyle)){
-		 	Pause();
-			Application.LoadLevel(0);
-		}
-		if (GUI.Button(Rect(300,245,200,60),"Instructions",buttonStyle)){
+		if(isMainMenu){
+			GUI.Label(Rect(300,50,200,40),"Paused",titleStyle);
 			
+			if (GUI.Button(Rect(300,100,200,60),"Resume",buttonStyle)){
+		 		Pause();
+			}
+			if (GUI.Button(Rect(300,172,200,60),"Main Menu",buttonStyle)){
+			 	Pause();
+				Application.LoadLevel(0);
+			}
+			if (GUI.Button(Rect(300,245,200,60),"Instructions",buttonStyle)){
+				reset();
+				isInstructions = true;
+			}
+			if (GUI.Button(Rect(300,317,200,60),"Options",buttonStyle)){
+			 	reset();
+				isOptions = true;
+			}
 		}
-		if (GUI.Button(Rect(300,317,200,60),"Options",buttonStyle)){
-		 
+		if(isInstructions){
+			GUI.Label(Rect(30,30,740,360),"ANDROID\nTilt the screen to move your spaceship\nTap on the screen to shoot once and hold town to keep firing.\n\nPC\nUse the arrow keys or ZQSD to move around\nPress spacebar to shoot\n\nEXPLINATION\nHelp the patient survive the infection by killing as many viruses as you can!\nDon't let them past you or touch you, as this will make his condition worse.\nYou can pick up upgrades by smashing into white bloodcells, but keep away from the red ones, wich will damage your ship, causing you to lose upgrades!",instructionStyle);
+			if (GUI.Button(Rect(300,400,200,60),"Back",buttonStyle)){
+				reset();
+				isMainMenu = true;
+			}
 		}
-		
+		if(isOptions){
+			GUI.Label(Rect(250,235,300,35),"Options coming soon!",optionsStyle);
+			if (GUI.Button(Rect(300,400,200,60),"Back",buttonStyle)){
+				reset();
+				isMainMenu = true;
+			}
+		}	
 	}
 	if(dead){
 	
-		var posted:boolean =false;	
+		var posted:boolean = false;	
 		
 		GUI.DrawTexture(Rect(0,0,800,480), bgTexture);
 		
 		GUI.Label(Rect(300,70,200,40),"You died!",titleStyle);
 		GUI.Label(Rect(300,120,200,40),"Score: " + totscore,scoreStyle);
 		
-		playerName = GUI.TextField (Rect (300, 160, 200, 30), playerName, 25);
+		playerName = GUI.TextField (Rect (300, 160, 200, 30), playerName, 25,textFieldStyle);
 		
 		if (GUI.Button(Rect(300,200,200,60),"Submit Score",buttonStyle)){
-	 		if (!posted){
-		 		if(playerName != ""){
+	 		if (posted == false){
+		 		if(playerName != "" && playerName != "Enter your name here!"){
 					posted = true;
+					textField = "Submitted!";
 					GetComponentInChildren(scoreHandler).SendMessage("postScore");
-					GUI.Label(Rect(Screen.width/2+100,Screen.height/2 + 50,100,20),"Submitted");
 				}
 			}
 		}
@@ -105,6 +131,7 @@ function OnGUI(){
 			score = 0;
 			Application.LoadLevel(0);
 		}
+		GUI.Label(Rect(550,230,200,30),textField,scoreStyle);
 	}
 }
 
@@ -123,4 +150,10 @@ function toggleDead(){
 		Time.timeScale = 0;
 		dead = true;
 	}
+}
+
+function reset(){
+	isMainMenu = false;
+	isOptions = false;
+	isInstructions = false;
 }
